246fff418a56d08c9c1f0126563da63a03fcb635
zero9178 <markus.boeck02@gmail.com>
2021-02-12 19:34:04 +0100

    Replaced uses of deprecated APIs (Icons and notifications)

--- src/main/java/icons/CPPCoverageIcons.java
+++
@@ -1,9 +0,0 @@
-package icons;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public interface CPPCoverageIcons {
-    Icon COVERAGE_RUN_ACTION = IconLoader.getIcon("/icons/CoverageRunAction.svg");
-}
--- src/main/kotlin/net/zero9178/cov/notification/CoverageNotification.kt
+++
@@ -1,8 +0,0 @@
-package net.zero9178.cov.notification
-
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
-
-object CoverageNotification {
-    val GROUP_DISPLAY_ID_INFO = NotificationGroup("Coverage", NotificationDisplayType.BALLOON, true)
-}
