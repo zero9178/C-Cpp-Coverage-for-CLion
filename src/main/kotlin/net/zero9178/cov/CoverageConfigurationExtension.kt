8057bb95a6b9fcd6dd5c11ce6b51469c0746f0ae
zero9178 <markus.boeck02@gmail.com>
2021-02-22 18:53:54 +0100

    Don't use the execution target currently active but the one that was used for the execution. If one were to start execution and then immediately change execution target we'd use the wrong generator

--- src/main/kotlin/net/zero9178/cov/CoverageConfigurationExtension.kt
+++ src/main/kotlin/net/zero9178/cov/CoverageConfigurationExtension.kt
@@ -18,6 +18,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.wm.ToolWindowManager
 import com.jetbrains.cidr.cpp.execution.CMakeAppRunConfiguration
+import com.jetbrains.cidr.cpp.execution.CMakeBuildProfileExecutionTarget
 import com.jetbrains.cidr.cpp.execution.coverage.CMakeCoverageBuildOptionsInstallerFactory
 import com.jetbrains.cidr.cpp.execution.testing.CMakeTestRunConfiguration
 import com.jetbrains.cidr.cpp.execution.testing.ctest.CidrCTestRunConfigurationData
@@ -101,7 +102,10 @@ class CoverageConfigurationExtension : CidrRunConfigurationExtensionBase() {
             return
         }
         val wasExplicitlyRequested = explicitlyRequested(configuration)
-        val executionTarget = ExecutionTargetManager.getInstance(configuration.project).activeTarget
+        val executionTarget = ExecutionTargetManager.getTargetsToChooseFor(configuration.project, configuration)
+            .filterIsInstance<CMakeBuildProfileExecutionTarget>().find {
+                it.profileName == configuration.targetAndConfigurationData?.configurationName
+            } ?: ExecutionTargetManager.getInstance(configuration.project).activeTarget
         handler.addProcessListener(object : ProcessAdapter() {
             override fun processTerminated(event: ProcessEvent) {
                 if (hasCompilerFlags(configuration) == false && wasExplicitlyRequested) {
@@ -223,7 +227,10 @@ class CoverageConfigurationExtension : CidrRunConfigurationExtensionBase() {
     }
 
     private fun hasCompilerFlags(configuration: CMakeAppRunConfiguration): Boolean? {
-        val executionTarget = ExecutionTargetManager.getInstance(configuration.project).activeTarget
+        val executionTarget = ExecutionTargetManager.getTargetsToChooseFor(configuration.project, configuration)
+            .filterIsInstance<CMakeBuildProfileExecutionTarget>().find {
+                it.profileName == configuration.targetAndConfigurationData?.configurationName
+            } ?: ExecutionTargetManager.getInstance(configuration.project).activeTarget
         val runConfiguration =
             if (configuration is CMakeTestRunConfiguration && isCTestInstalled() && configuration.testData is CidrCTestRunConfigurationData) {
                 // Returning null here means we don't know. How this is handled depends on the caller
--- src/main/kotlin/net/zero9178/cov/data/LLVMCoverageGenerator.kt
+++ src/main/kotlin/net/zero9178/cov/data/LLVMCoverageGenerator.kt
@@ -26,6 +26,7 @@ import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.io.delete
 import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace
 import com.jetbrains.cidr.cpp.execution.CMakeAppRunConfiguration
+import com.jetbrains.cidr.cpp.execution.CMakeBuildProfileExecutionTarget
 import com.jetbrains.cidr.cpp.execution.testing.CMakeTestRunConfiguration
 import com.jetbrains.cidr.cpp.execution.testing.ctest.CidrCTestRunConfigurationData
 import com.jetbrains.cidr.cpp.toolchains.CPPEnvironment
@@ -62,7 +63,10 @@ class LLVMCoverageGenerator(
         environment: CPPEnvironment,
         cmdLine: GeneralCommandLine
     ) {
-        val executionTarget = ExecutionTargetManager.getInstance(configuration.project).activeTarget
+        val executionTarget = ExecutionTargetManager.getTargetsToChooseFor(configuration.project, configuration)
+            .filterIsInstance<CMakeBuildProfileExecutionTarget>().find {
+                it.profileName == configuration.targetAndConfigurationData?.configurationName
+            } ?: ExecutionTargetManager.getInstance(configuration.project).activeTarget
         val config = CMakeWorkspace.getInstance(configuration.project).getCMakeConfigurationFor(
             configuration.getResolveConfiguration(executionTarget)
         ) ?: return
@@ -564,8 +568,8 @@ class LLVMCoverageGenerator(
                 listOf(
                     myLLVMProf,
                     "merge",
-                "-output=${tempFile}"
-            ) + files.map { environment.toEnvPath(it.absolutePath) })
+                    "-output=${tempFile}"
+                ) + files.map { environment.toEnvPath(it.absolutePath) })
                 .withWorkDirectory(environment.toEnvPath(configurationGenerationDir.absolutePath)),
             indicator,
             -1
