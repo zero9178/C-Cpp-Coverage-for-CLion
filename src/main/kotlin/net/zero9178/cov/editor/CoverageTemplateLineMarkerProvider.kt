60c3715b1dbb2a1a53746e8cc765e50625d4caa0
zero9178 <markus.boeck02@gmail.com>
2021-03-07 17:53:44 +0100

    Changed the action when changing template instantiations to a balloon containing a combo box

--- src/main/kotlin/net/zero9178/cov/editor/CoverageTemplateLineMarkerProvider.kt
+++ src/main/kotlin/net/zero9178/cov/editor/CoverageTemplateLineMarkerProvider.kt
@@ -5,32 +5,33 @@ import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.parents
 import com.intellij.ui.awt.RelativePoint
-import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.SelectionAwareListCellRenderer
-import com.intellij.util.ui.JBUI
 import com.jetbrains.cidr.lang.parser.OCTokenTypes
 import com.jetbrains.cidr.lang.psi.OCFunctionDefinition
 import com.jetbrains.cidr.lang.psi.OCLambdaExpression
 import com.jetbrains.cidr.lang.psi.OCMethod
 import icons.CPPCoverageIcons
 import net.zero9178.cov.util.toCP
-import java.awt.MouseInfo
 import java.awt.Point
-import javax.swing.SwingConstants
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
+import java.awt.event.HierarchyBoundsAdapter
+import java.awt.event.HierarchyEvent
+import kotlin.math.min
+
+private const val MAX_COMBO_BOX_WIDTH = 400
 
 class CoverageTemplateLineMarkerProvider : LineMarkerProvider {
-
-    init {
-        print("test")
-    }
 
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         if (element !is LeafPsiElement || element.elementType !== OCTokenTypes.LBRACE) {
@@ -71,7 +72,8 @@ class CoverageTemplateLineMarkerProvider : LineMarkerProvider {
         }) {
             override fun createGutterRenderer(): GutterIconRenderer {
                 return object : LineMarkerGutterIconRenderer<PsiElement>(this) {
-                    override fun getClickAction(): AnAction = TemplateInstantiationsChoosePopup(group, highlighter)
+                    override fun getClickAction(): AnAction =
+                        TemplateInstantiationsChoosePopup(group, highlighter, this)
 
                     override fun isNavigateAction(): Boolean {
                         return true
@@ -83,32 +85,54 @@ class CoverageTemplateLineMarkerProvider : LineMarkerProvider {
 
     private class TemplateInstantiationsChoosePopup(
         val group: CoverageHighlighter.HighlightFunctionGroup,
-        val highlighter: CoverageHighlighter
+        val highlighter: CoverageHighlighter,
+        val renderer: GutterIconRenderer
     ) : AnAction() {
         override fun actionPerformed(e: AnActionEvent) {
-            val popUp = JBPopupFactory.getInstance()
-                .createPopupChooserBuilder(group.functions.keys.toList()).apply {
-                    setRenderer(SelectionAwareListCellRenderer {
-                        val text = StringUtil.first(it, 100, true)
-                        JBLabel(text, SwingConstants.LEFT).apply {
-                            border = JBUI.Borders.empty(2)
-                        }
-                    })
-                    setItemChosenCallback {
-                        highlighter.changeActive(group, it)
-                    }
-                    setTitle("Template Instantiation")
-                }.createPopup()
-            val editor = e.getData(CommonDataKeys.EDITOR) ?: return
-            val location = MouseInfo.getPointerInfo().location
-            popUp.show(
-                RelativePoint(
-                    Point(
-                        location.x - popUp.content.preferredSize.width / 2,
-                        location.y - popUp.content.preferredSize.height / 2
-                    )
-                ).getPointOn(editor.contentComponent)
-            )
+            val editor = e.getData(CommonDataKeys.EDITOR) as? EditorEx ?: return
+            val gutterComponent = editor.gutterComponentEx
+            var point = gutterComponent.getCenterPoint(renderer)
+            if (point == null) { // disabled gutter icons for example
+                point = Point(
+                    gutterComponent.width,
+                    editor.visualPositionToXY(editor.caretModel.visualPosition).y + editor.lineHeight / 2
+                )
+            }
+            val comboBox = ComboBox(group.functions.keys.toTypedArray())
+            val maxLength = group.functions.keys.maxOf { it.length }
+            comboBox.prototypeDisplayValue = "".padStart(min(maxLength, MAX_COMBO_BOX_WIDTH))
+            comboBox.setMinLength(100)
+            comboBox.addActionListener {
+                if (Disposer.isDisposed(group)) {
+                    return@addActionListener
+                }
+                highlighter.changeActive(group, comboBox.item)
+            }
+            val balloon = JBPopupFactory.getInstance().createDialogBalloonBuilder(comboBox, null).apply {
+                setHideOnClickOutside(true)
+                setCloseButtonEnabled(false)
+                setAnimationCycle(0)
+                setBlockClicksThroughBalloon(true)
+            }.createBalloon()
+            val moveListener = object : ComponentAdapter() {
+                override fun componentMoved(e: ComponentEvent?) {
+                    balloon.hide()
+                }
+            }
+            gutterComponent.addComponentListener(moveListener)
+            Disposer.register(balloon) {
+                gutterComponent.removeComponentListener(moveListener)
+            }
+            val hierarchyListener = object : HierarchyBoundsAdapter() {
+                override fun ancestorMoved(e: HierarchyEvent?) {
+                    balloon.hide()
+                }
+            }
+            gutterComponent.addHierarchyBoundsListener(hierarchyListener)
+            Disposer.register(balloon) {
+                gutterComponent.removeHierarchyBoundsListener(hierarchyListener)
+            }
+            balloon.show(RelativePoint(gutterComponent, point), Balloon.Position.above)
         }
     }
 
