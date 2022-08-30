798e85129061279cea03be22717371df49c0e5f1
zero9178 <markus.boeck02@gmail.com>
2022-08-30 13:46:46 +0200

    Revert "2022.2.1 Build"
    This reverts commit ba9191aa773e55f9ad32ca0fc89dd743fd2815e5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.8.1"
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.intellij") version "1.1.6"
+    id("org.jetbrains.kotlin.jvm") version "1.5.0"
 }
 
 group = "net.zero9178"
@@ -18,24 +18,24 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.beust:klaxon:5.2")
     implementation("com.beust:klaxon-jackson:1.0.1")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0")
     implementation("com.github.h0tk3y.betterParse:better-parse-jvm:0.4.2")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.2.1")
+    version.set("213.3714-EAP-CANDIDATE-SNAPSHOT")
     plugins.set(listOf("com.intellij.clion", "com.intellij.cidr.base", "com.intellij.cidr.lang", "clion-ctest"))
     type.set("CL")
 }
 
 tasks {
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "11"
     }
 
     patchPluginXml {
